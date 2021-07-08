86745ed555f1537fdfef14e6c7e6f2aa44d0bfe6
Daniil Sychev <sych94@yandex.ru>
2021-07-08 15:28:46 +0300

    'about'

--- src/components/App/AboutPage/AboutPage.tsx
+++ src/components/App/AboutPage/AboutPage.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import classes from './AboutPage.module.scss';
 
 const AboutPage = () => (
   <div className={classes.AboutWrapper}>
     <div className={classes.PhotoWrapper}>
-      {/* <img alt="me" src={photo} /> */}
+      <img className="image-container" alt="me" src="https://picsum.photos/600/300" />
     </div>
     <h1>Daniil Sychev</h1>
     <p style={{ textAlign: 'center' }}>
       {/* eslint-disable-next-line */}
       At this project I have implemented GET's from <a href="https://jsonplaceholder.typicode.com/">jsonplaseholder</a>
+      <br />
+      App built using antd, typrscript, react, mobx
     </p>
   </div>
 );
